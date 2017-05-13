2142b80e76319587c274694272b1478e77da6d72
ravikishorethella <ravikishorethella@gmail.com>
2017-05-13 00:27:44 -0400

    printing each character in a string

--- file.js
+++ file.js
@@ -1,20 +1,38 @@
-// create a number
-var original = 4;
+////  Guesing the random number
+//
+//// create a number
+//var original = 4;
+//
+////ask the user to enter a number
+//var number = Number(prompt("Guess a number"));
+//
+//// check if the condition is true
+//if(number === original){
+//    alert("You guessed right");
+//}
+// else if(number >= original){
+//     alert("too high");
+// }
+//else if(number <= original){
+//    alert("too low");
+//}
+//else{
+//    alert("unknown error");
+//}
 
-//ask the user to enter a number
-var number = Number(prompt("Guess a number"));
+//==============================================================
 
-// check if the condition is true
-if(number === original){
-    alert("You guessed right");
-}
- else if(number >= original){
-     alert("too high");
- }
-else if(number <= original){
-    alert("too low");
-}
-else{
-    alert("unknown error");
-}
+// print each character in a string
 
+// creating a string named javascript
+var string = "javascript";
+
+// assigning the initial count
+var count = 0;
+
+//iterating over the loop to find the characters in the string
+while(count<string.length){
+    console.log(string[count]);
+    count++;
+}
+//================================================================
