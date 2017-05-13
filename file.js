b322020f2f025940a7cbfdba4c8e6f2693fd089f
ravikishorethella <ravikishorethella@gmail.com>
2017-05-13 00:05:06 -0400

    js code added

--- file.js
+++ file.js
@@ -1,1 +1,20 @@
-alert("hello")
+// create a number
+var original = 4;
+
+//ask the user to enter a number
+var number = Number(prompt("Guess a number"));
+
+// check if the condition is true
+if(number === original){
+    alert("You guessed right");
+}
+ else if(number >= original){
+     alert("too high");
+ }
+else if(number <= original){
+    alert("too low");
+}
+else{
+    alert("unknown error");
+}
+
